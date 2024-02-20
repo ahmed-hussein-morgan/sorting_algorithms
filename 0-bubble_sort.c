90d90f1261c2f6e73b0b752763f22ae5541f8e65
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-20 10:59:40 +0000

    update the code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,8 +21,16 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array,size);
 			}
 			/*print_array(array,size);*/
+			/*print_array(array,size);
+			else
+				{
+					print_array(array, size);
+					break;
+				}*/
 		}
+		print_array(array,size);
 	}
 }
