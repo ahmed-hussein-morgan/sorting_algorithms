5b75a462e3ae06263ef044e373c7d6e2ee87ab24
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-20 11:25:51 +0000

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,10 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (size == 0 || !array)
+	{
+		return;
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
