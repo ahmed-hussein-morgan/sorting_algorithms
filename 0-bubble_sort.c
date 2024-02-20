c7e30e17110352ae98a1e9eebff9a55157ea4ecf
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-20 10:42:55 +0000

    bubble sorting  - try

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Entry point
+ * @array: the array of elements
+ * @size: the size of array
+ * Return: No return value expected
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+			print_array(array,size);
+		}
+	}
+}
