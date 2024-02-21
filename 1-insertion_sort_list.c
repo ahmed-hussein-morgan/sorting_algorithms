91388ef985e48c7b120de89288c47683d59e72b0
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-21 02:43:04 +0000

    selection sort - anser

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Entry point
+ * @list: the number of arguments
+ * Return: No return value expected
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,18 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Entry point
+ * @array: the array of elements
+ * @size: the size of array
+ * Return: No return value expected
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	if (size == 0 || !array)
+	{
+		return;
+	}
+}
