ca493e95443656332e1c63644ad84618df85a65a
Ahemd Hussein Morgan <136130399+ahmed-hussein-morgan@users.noreply.github.com>
2024-02-25 02:57:58 +0000

    01

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,5 +8,36 @@
 
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *curr, *temp, *sorted;
 
+	if (list == NULL || *list == NULL)
+		return;
+
+	curr = (*list)->next;
+	while (curr)
+	{
+		sorted = curr;
+		curr = curr->next;
+
+		while (sorted && sorted->prev && sorted->prev->n > sorted->n)
+		{
+			temp = sorted->prev;
+			sorted->prev = temp->prev;
+			temp->next = sorted->next;
+
+			if (temp->prev)
+				temp->prev->next = sorted;
+
+			if (sorted->next)
+				sorted->next->prev = temp;
+
+			temp->prev = sorted;
+			sorted->next = temp;
+
+			if (!sorted->prev)
+				*list = sorted;
+
+			print_list(*list);
+		}
+	}
 }
